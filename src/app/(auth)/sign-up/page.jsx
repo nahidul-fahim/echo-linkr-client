f5f8c385fd18ba136cd1b8c6520ce8117c30d754
nahidul-fahim <nahidulfahim.official@gmail.com>
2024-02-18 23:47:41 +0600

    created auth folder

---
+++ src/app/(auth)/sign-up/page.jsx
@@ -0,0 +1,11 @@
+
+
+const page = () => {
+    return (
+        <div>
+            <h2 className="text-xl">Sign up page</h2>
+        </div>
+    );
+};
+
+export default page;
--- src/app/page.js
+++ src/app/page.js
@@ -4,7 +4,7 @@ import Image from "next/image";
 export default function Home() {
 
   return (
-    <main className="h-[100vh] flex flex-col justify-center items-center gap-5">
+    <main className="h-screen flex flex-col justify-center items-center gap-5">
       <h2 className="text-4xl font-bold text-center">New Heading Here</h2>
       <Button variant="destructive">Shadcn Button</Button>
     </main>
