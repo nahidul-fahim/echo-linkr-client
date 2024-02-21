ab8b5f6ba4b1f241c65e5acca582745f5ec37d0f
nahidul-fahim <nahidulfahim.official@gmail.com>
2024-02-21 11:56:49 +0600

    created sign up page and installed axios, json web token, mongoose

--- src/app/(auth)/sign-in/page.jsx
+++ src/app/(auth)/sign-in/page.jsx
@@ -1,18 +1,11 @@
 "use client"
 
-import { usePathname, useRouter } from "next/navigation";
-
-
 const SignIn = () => {
-
-    const pathName = usePathname();
-    const router = useRouter();
-    console.log(router)
 
 
     return (
-        <div>
-            <h2>Sign In page</h2>
+        <div className="container">
+            
         </div>
     );
 };
--- src/app/(auth)/sign-up/page.jsx
+++ src/app/(auth)/sign-up/page.jsx
@@ -2,8 +2,29 @@
 
 const page = () => {
     return (
-        <div>
-            <h2 className="text-xl">Sign up page</h2>
+        <div className="container mx-auto p-5 flex flex-col justify-center items-center min-h-screen">
+            <div className="w-[40%] border-[1px] border-border flex flex-col justify-center items-center gap-8 py-14 rounded-[20px] shadow-border">
+                <h1 className="text-2xl font-semibold text-foreground">Unlock a World of Connections!</h1>
+
+                {/* registration form */}
+                <form className="w-full flex flex-col justify-center items-center gap-5">
+                    {/* Full name */}
+                    <input type="text" name="name" id="name" placeholder="Full name" className="border-[1px] px-4 py-2 rounded focus:outline-none focus:border-lightBlack w-2/3" />
+
+                    {/* Email */}
+                    <input type="email" name="email" id="email" placeholder="Email address" className="border-[1px] px-4 py-2 rounded focus:outline-none focus:border-lightBlack w-2/3" />
+
+                    {/* Username */}
+                    <input type="text" name="userName" id="userName" placeholder="User name" className="border-[1px] px-4 py-2 rounded focus:outline-none focus:border-lightBlack w-2/3" />
+
+                    {/* Password */}
+                    <input type="password" name="password" id="password" placeholder="Password" className="border-[1px] px-4 py-2 rounded focus:outline-none focus:border-lightBlack w-2/3" />
+
+                    {/* Submit button */}
+                    <input type="submit" value={"Sign Up"} className="bg-foreground text-background font-medium hover:bg-background hover:text-foreground duration-500 border-[1px] border-transparent px-4 py-2 rounded focus:outline-none hover:border-lightBlack cursor-pointer w-2/3" />
+
+                </form>
+            </div>
         </div>
     );
 };
