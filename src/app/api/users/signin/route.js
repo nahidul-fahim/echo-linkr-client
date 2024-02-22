bfeb023ac7f3535ce2d6cbc3a1791f5b990f6fca
nahidul-fahim <nahidulfahim.official@gmail.com>
2024-02-22 12:13:06 +0600

    creating token and setting the token to browser cookies

--- src/app/api/users/signin/route.js
+++ src/app/api/users/signin/route.js
@@ -1,6 +1,7 @@
 import { connect } from "@/dbConfig/dbConfig";
 import User from "@/models/userModel"
 import { NextRequest, NextResponse } from "next/server";
+import jwt from "jsonwebtoken";
 
 // connect to the database
 connect();
@@ -17,14 +18,30 @@ export async function POST(request) {
         }
 
         console.log("Sign in user exists:", true);
-        console.log("existing user", existingUser);
         if (password === existingUser.password) {
-            return NextResponse.json({ message: 'Sign in successful!', success: true })
+
+            // create token data
+            const tokenData = {
+                id: existingUser?._id,
+                userName: existingUser?.userName,
+                email: existingUser?.email,
+            }
+
+            // create JSON web token
+            const token = await jwt.sign(tokenData, process.env.ACCESS_WEB_TOKEN, { expiresIn: '1h' })
+
+            // send the response to the front end
+            const response = NextResponse.json({
+                message: 'Sign in successful!',
+                success: true
+            })
+            // set the token to browser cookies
+            response.cookies.set("token", token, { httpOnly: true })
+            return response;
         }
         else {
             return NextResponse.json({ message: 'Check your email or password!', success: false })
         }
-
     }
     catch (error) {
         return NextResponse.json({ error: error }, { status: 500 })
